b890e4c1e86988fa174e1e420445f3c94c551374
Aleskey <colthands@protonmail.com>
2024-03-23 14:47:57 +0400

    [refactor] Update article button button

--- components/ui/Button.vue
+++ components/ui/Button.vue
@@ -25,19 +25,19 @@ button
     font-weight: 800
     text-transform: uppercase
     position: relative
-    transition: all ease 100ms
+    svg
+        fill: variables.$gray
     &::after
         content: ""
         width: calc(100% + 2px)
         height: calc(100% + 2px)
         position: absolute
         top: 3px
-        left: 3.5px
+        left: 3px
         border-right: 1.5px solid variables.$white
         border-bottom: 1.5px solid variables.$white
         border-radius: 2px
         border-bottom-right-radius: 3px
-        transition-delay: 50ms
     &:active
         transform: translate(4px, 4px)
         box-shadow: 0px 0px 0px 0px variables.$gray
@@ -55,4 +55,6 @@ button
         justify-content: center
         align-items: center
         font-size: 1.25em
+        border-top-right-radius: 0.5px
+        border-bottom-right-radius: 0.5px
 </style>
