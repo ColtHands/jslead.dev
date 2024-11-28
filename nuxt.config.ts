5cf87edefabbf59b6c1839aee6934855e7b06b35
Aleskey <colthands@protonmail.com>
2024-11-29 00:35:41 +0400

    [feat] fix auto identation nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,48 +1,38 @@
-const websiteUrl = 'https://colthands.dev'
-
 export default defineNuxtConfig({
-  modules: [
-      '@nuxt/content',
-      '@nuxthq/studio',
-      '@nuxt/fonts'
-  ],
-
-  devtools: { enabled: false },
-
-  app: {
-      pageTransition: {
-          name: 'page',
-          mode: 'default'
-      }
-  },
-
-  typescript: {
-      strict: true
-  },
-
-  routeRules: {
-      '/blog/**': { swr: true }
-  },
-
-  runtimeConfig: {
-      public: {
-          posthogPublicKey: 'phc_h4lVplILWIY7UlOfGIp9vjHlwHOA1697FEuiPAuwc2L',
-          posthogHost: 'https://app.posthog.com',
-          websiteUrl
-      }
-  },
-
-  content: {
-      highlight: {
-          theme: 'github-dark'
-      },
-      markdown: {
-          toc: {
-              depth: 2,
-              searchDepth: 2
-          }
-      }
-  },
-
-  compatibilityDate: '2024-11-28'
+    modules: [
+        '@nuxt/content',
+        '@nuxthq/studio',
+        '@nuxt/fonts'
+    ],
+    devtools: { enabled: false },
+    app: {
+        pageTransition: {
+            name: 'page',
+            mode: 'default'
+        }
+    },
+    typescript: {
+        strict: true
+    },
+    routeRules: {
+        '/blog/**': { swr: true }
+    },
+    runtimeConfig: {
+        public: {
+            posthogPublicKey: 'phc_h4lVplILWIY7UlOfGIp9vjHlwHOA1697FEuiPAuwc2L',
+            posthogHost: 'https://app.posthog.com',
+        }
+    },
+    content: {
+        highlight: {
+            theme: 'github-dark'
+        },
+        markdown: {
+            toc: {
+                depth: 2,
+                searchDepth: 2
+            }
+        }
+    },
+    compatibilityDate: '2024-11-28'
 })
