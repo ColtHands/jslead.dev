df5805d3595f78dd120e972cb1bbc5e4d82a4cce
Aleksei <aleksei@jslead.dev>
2025-05-31 10:09:23 +0400

    [test] Add iphone index page screenshot tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,12 @@
 import { fileURLToPath } from 'node:url'
 import { defineConfig, devices } from '@playwright/test'
 import type { ConfigOptions } from '@nuxt/test-utils/playwright'
-import { isCI, isWindows } from 'std-env'
+import { isCI } from 'std-env'
 
 const devicesToTest = [
-    'Desktop Chrome'
+    'Desktop Chrome',
+    'iPhone 15',
+    'iPhone 15 Landscape'
 ] satisfies Array<string | typeof devices[string]>
 
 export default defineConfig<ConfigOptions>({
@@ -13,7 +15,7 @@ export default defineConfig<ConfigOptions>({
     forbidOnly: !!isCI,
     retries: isCI ? 2 : 0,
     workers: isCI ? 1 : undefined,
-    timeout: isWindows ? 60000 : undefined,
+    timeout: 60000,
     reporter: 'html',
     use: {
         trace: 'on-first-retry',
